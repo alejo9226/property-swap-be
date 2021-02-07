402c3f01a5fee303eba289b827e2703210c73057
Alejandro Alfaro <alejo9226@gmail.com>
2021-02-07 13:25:21 -0500

    Add get single property route

--- src/routes/property.ts
+++ src/routes/property.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import auth from '../utils/auth'
-import { addProperty, getProperties } from '../controllers/property.controller'
+import { addProperty, getProperties, getSingleProperty } from '../controllers/property.controller'
 
 const router = Router()
 
 router.use(auth)
 router.route('/add').post(addProperty)
+
 router.route('/').get(getProperties)
+router.route('/:propertyid').get(getSingleProperty)
 
 export default router;
